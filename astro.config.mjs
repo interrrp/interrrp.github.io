13b58a60410cc66c50667098929f1d569bd20f14
interrrp <int@addictedto.coffee>
2024-05-15 16:50:11 +0800

    Create header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon()],
+});
