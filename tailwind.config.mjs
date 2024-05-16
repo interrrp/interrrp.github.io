e0474ddd9c4aa7a82f5bdd2b626938b81868b810
interrrp <int@addictedto.coffee>
2024-05-16 16:49:56 +0800

    Set theme to Ayu Dark

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ayu: {
+          bg: "#0b0e14",
+          fg: "#cccac2",
+          muted: "#b8cfe680",
+          muter: "#b8cfe670",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
